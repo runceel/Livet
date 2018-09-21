eb00853fb4cf2bf1d6736add6df0a637a3448b3d
Kazuki Ota <kaota@microsoft.com>
2018-09-21 12:24:19 +0900

    add ItemTemplates

---
+++ Templates/ItemTemplates/Livet.ItemTemplateExtension/ItemTemplateWizard.cs
@@ -0,0 +1,51 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+namespace Livet.ItemTemplateExtension
+{
+    public class ItemTemplateWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                var dte = (_DTE)automationObject;
+                var project = (Project)((object[])dte.ActiveSolutionProjects)[0];
+                string defaultNamespae = null;
+
+                foreach (Property property in project.Properties)
+                {
+                    if (property.Name.ToLower() == "defaultnamespace")
+                    {
+                        defaultNamespae = (string)property.Value;
+                    }
+                }
+
+                replacementsDictionary.Add("$projectrootnamespace$", defaultNamespae);
+            }
+            catch { }
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
---
+++ Templates/ItemTemplates/LivetInteractionMessageAction.CSharp/Action.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Interactivity;
+
+using Livet.Messaging;
+using Livet.Behaviors.Messaging;
+
+
+namespace $rootnamespace$
+{
+	//Tはこのアクションがアタッチできる型を表します。
+	//この場合はこのアクションはFrameworkElementにアタッチできます。
+    public class $safeitemname$ : InteractionMessageAction<FrameworkElement>
+    {
+        protected override void InvokeAction(Livet.Messaging.InteractionMessage m)
+        {
+            ////このアクションが対応するメッセージに変換します。
+            //var confirmMessage = m as ConfirmationMessage;
+
+            //if (confirmMessage != null)
+            //{
+            //    //受け取ったメッセージの情報を使用して実行したいアクションを実行します。
+            //    var result = MessageBox.Show(
+            //        confirmMessage.Text,
+            //        confirmMessage.Caption,
+            //        confirmMessage.Button,
+            //        confirmMessage.Image
+            //        );
+
+            //    //戻り値のあるメッセージの場合、戻り値を設定します。
+            //    if (result == MessageBoxResult.Yes || result == MessageBoxResult.OK)
+            //    {
+            //        confirmMessage.Response = true;
+            //    }
+            //    else
+            //    {
+            //        confirmMessage.Response = false;
+            //    }
+            //}
+        }
+    }
+}
