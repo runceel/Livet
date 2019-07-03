778cb2224b57060592341bbace46163d5fad8caa
terasato <terasato@users.noreply.github.com>
2019-07-03 16:56:34 +0900

    Update EventListener.cs

--- Livet.Shared/EventListeners/EventListener.cs
+++ Livet.Shared/EventListeners/EventListener.cs
@@ -1,38 +1,50 @@
 ﻿using System;
+using Livet.Annotations;
 
 namespace Livet.EventListeners
 {
     /// <summary>
-    /// 汎用イベントリスナオブジェクトです。
+    ///     汎用イベントリスナオブジェクトです。
     /// </summary>
     /// <typeparam name="THandler">イベントハンドラーの型</typeparam>
     public class EventListener<THandler> : IDisposable where THandler : class
     {
-        private THandler _handler;
-        private Action<THandler> _add;
-        private Action<THandler> _remove;
+        private readonly bool _initialized;
+        [CanBeNull] private Action<THandler> _add;
         private bool _disposed;
-
-        private bool _initialized;
+        [CanBeNull] private THandler _handler;
+        [CanBeNull] private Action<THandler> _remove;
 
         /// <summary>
-        /// コンストラクタ
+        ///     コンストラクタ
         /// </summary>
         /// <param name="add">h => obj.Event += > h の様な形でイベントの購読を登録するためのAction。hはTHandler型です。</param>
         /// <param name="remove">h => obj.Event -= > h の様な形でイベントの購読を解除するためのAction。hはTHandler型です。</param>
         /// <param name="handler">イベントを受信した際に行いたいアクション</param>
-        public EventListener(Action<THandler> add, Action<THandler> remove, THandler handler)
+        public EventListener([NotNull] Action<THandler> add, [NotNull] Action<THandler> remove, [NotNull] THandler handler)
         {
+            if (add == null) throw new ArgumentNullException(nameof(add));
+            if (remove == null) throw new ArgumentNullException(nameof(remove));
+            if (handler == null) throw new ArgumentNullException(nameof(handler));
+
             Initialize(add, remove, handler);
             _initialized = true;
         }
 
         protected EventListener()
         {
-
         }
 
-        protected void Initialize(Action<THandler> add, Action<THandler> remove, THandler handler)
+        /// <summary>
+        ///     イベントハンドラの登録を解除します。
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Initialize([NotNull] Action<THandler> add, [NotNull] Action<THandler> remove, [NotNull] THandler handler)
         {
             if (_initialized) return;
 
@@ -44,19 +56,7 @@ namespace Livet.EventListeners
 
         protected void ThrowExceptionIfDisposed()
         {
-            if (_disposed)
-            {
-                throw new ObjectDisposedException("EventListener");
-            }
-        }
-
-        /// <summary>
-        /// イベントハンドラの登録を解除します。
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            if (_disposed) throw new ObjectDisposedException("EventListener");
         }
 
         protected virtual void Dispose(bool disposing)
@@ -65,14 +65,13 @@ namespace Livet.EventListeners
 
             if (disposing)
             {
-                _remove(_handler);
+                _remove?.Invoke(_handler);
                 _add = null;
                 _remove = null;
                 _handler = null;
             }
+
             _disposed = true;
         }
     }
-
-
 }
