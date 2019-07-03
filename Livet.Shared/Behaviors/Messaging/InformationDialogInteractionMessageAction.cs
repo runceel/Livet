f4389c850170fef9ec454d65538b8588f63ded29
terasato <terasato@users.noreply.github.com>
2019-07-03 16:45:56 +0900

    Update InformationDialogInteractionMessageAction.cs

--- Livet.Shared/Behaviors/Messaging/InformationDialogInteractionMessageAction.cs
+++ Livet.Shared/Behaviors/Messaging/InformationDialogInteractionMessageAction.cs
@@ -1,25 +1,22 @@
-﻿using Livet.Messaging;
-using System.Windows;
+﻿using System.Windows;
+using Livet.Messaging;
 
 namespace Livet.Behaviors.Messaging
 {
     /// <summary>
-    /// 情報ダイアログを表示するアクションです。InformationMessageに対応します。
+    ///     情報ダイアログを表示するアクションです。InformationMessageに対応します。
     /// </summary>
     public class InformationDialogInteractionMessageAction : InteractionMessageAction<FrameworkElement>
     {
         protected override void InvokeAction(InteractionMessage message)
         {
             if (message is InformationMessage informationMessage)
-            {
                 MessageBox.Show(
                     informationMessage.Text,
                     informationMessage.Caption,
                     MessageBoxButton.OK,
                     informationMessage.Image
-                    );
-            }
-
+                );
         }
     }
 }
