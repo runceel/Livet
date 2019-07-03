f3fb19056d5586ee3ff08e5197dec6af43c06baa
terasato <terasato@users.noreply.github.com>
2019-07-03 16:53:55 +0900

    Update PropertyChangedEventListener.cs

--- Livet.Shared/EventListeners/PropertyChangedEventListener.cs
+++ Livet.Shared/EventListeners/PropertyChangedEventListener.cs
@@ -1,40 +1,66 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq.Expressions;
+using Livet.Annotations;
 
 namespace Livet.EventListeners
 {
     /// <summary>
-    /// INotifyPropertyChanged.PropertyChangedを受信するためのイベントリスナです。
+    ///     INotifyPropertyChanged.PropertyChangedを受信するためのイベントリスナです。
     /// </summary>
-    public sealed class PropertyChangedEventListener : EventListener<PropertyChangedEventHandler>,IEnumerable<KeyValuePair<string,List<PropertyChangedEventHandler>>>
+    public sealed class PropertyChangedEventListener : EventListener<PropertyChangedEventHandler>,
+        IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>
     {
-        private AnonymousPropertyChangedEventHandlerBag _bag;
+        [NotNull] private readonly AnonymousPropertyChangedEventHandlerBag _bag;
 
         /// <summary>
-        /// コンストラクタ
+        ///     コンストラクタ
         /// </summary>
         /// <param name="source">INotifyPropertyChangedオブジェクト</param>
-        public PropertyChangedEventListener(INotifyPropertyChanged source)
+        public PropertyChangedEventListener([NotNull] INotifyPropertyChanged source)
         {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
             _bag = new AnonymousPropertyChangedEventHandlerBag(source);
-            Initialize(h => source.PropertyChanged += h, h => source.PropertyChanged -= h, (sender, e) => _bag.ExecuteHandler(e));
+            Initialize(h => source.PropertyChanged += h, h => source.PropertyChanged -= h,
+                (sender, e) => _bag.ExecuteHandler(e));
         }
 
         /// <summary>
-        /// コンストラクタ。リスナのインスタンスの作成と同時にハンドラを一つ登録します。
+        ///     コンストラクタ。リスナのインスタンスの作成と同時にハンドラを一つ登録します。
         /// </summary>
         /// <param name="source">INotifyPropertyChangedオブジェクト</param>
         /// <param name="handler">PropertyChangedイベントハンドラ</param>
-        public PropertyChangedEventListener(INotifyPropertyChanged source, PropertyChangedEventHandler handler)
+        public PropertyChangedEventListener([NotNull] INotifyPropertyChanged source,
+            [NotNull] PropertyChangedEventHandler handler)
         {
-           _bag = new AnonymousPropertyChangedEventHandlerBag(source,handler);
-           Initialize(h => source.PropertyChanged += h, h => source.PropertyChanged -= h, (sender, e) => _bag.ExecuteHandler(e));
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (handler == null) throw new ArgumentNullException(nameof(handler));
+
+            _bag = new AnonymousPropertyChangedEventHandlerBag(source, handler);
+            Initialize(h => source.PropertyChanged += h, h => source.PropertyChanged -= h,
+                (sender, e) => _bag.ExecuteHandler(e));
+        }
+
+        IEnumerator<KeyValuePair<string, List<PropertyChangedEventHandler>>>
+            IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>.GetEnumerator()
+        {
+            ThrowExceptionIfDisposed();
+            return
+                ((IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>) _bag)
+                .GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            ThrowExceptionIfDisposed();
+            return ((IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>) _bag).GetEnumerator();
         }
 
         /// <summary>
-        /// このリスナインスタンスに新たなハンドラを追加します。
+        ///     このリスナインスタンスに新たなハンドラを追加します。
         /// </summary>
         /// <param name="handler">PropertyChangedイベントハンドラ</param>
         public void RegisterHandler(PropertyChangedEventHandler handler)
@@ -44,39 +70,25 @@ namespace Livet.EventListeners
         }
 
         /// <summary>
-        /// このリスナインスタンスにプロパティ名でフィルタリング済のハンドラを追加します。
+        ///     このリスナインスタンスにプロパティ名でフィルタリング済のハンドラを追加します。
         /// </summary>
         /// <param name="propertyName">ハンドラを登録したいPropertyChangedEventArgs.PropertyNameの名前</param>
         /// <param name="handler">propertyNameで指定されたプロパティ用のPropertyChangedイベントハンドラ</param>
         public void RegisterHandler(string propertyName, PropertyChangedEventHandler handler)
         {
             ThrowExceptionIfDisposed();
-            _bag.RegisterHandler(propertyName,handler);
+            _bag.RegisterHandler(propertyName, handler);
         }
 
         /// <summary>
-        /// このリスナインスタンスにプロパティ名でフィルタリング済のハンドラを追加します。
+        ///     このリスナインスタンスにプロパティ名でフィルタリング済のハンドラを追加します。
         /// </summary>
         /// <param name="propertyExpression">() => プロパティ形式のラムダ式</param>
         /// <param name="handler">propertyExpressionで指定されたプロパティ用のPropertyChangedイベントハンドラ</param>
         public void RegisterHandler<T>(Expression<Func<T>> propertyExpression, PropertyChangedEventHandler handler)
         {
             ThrowExceptionIfDisposed();
-            _bag.RegisterHandler(propertyExpression,handler);
-        }
-
-        IEnumerator<KeyValuePair<string, List<PropertyChangedEventHandler>>> IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>.GetEnumerator()
-        {
-            ThrowExceptionIfDisposed();
-            return
-                ((IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>) _bag)
-                    .GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            ThrowExceptionIfDisposed();
-            return ((IEnumerable<KeyValuePair<string, List<PropertyChangedEventHandler>>>)_bag).GetEnumerator();
+            _bag.RegisterHandler(propertyExpression, handler);
         }
 
         public void Add(PropertyChangedEventHandler handler)
@@ -88,33 +100,27 @@ namespace Livet.EventListeners
         public void Add(string propertyName, PropertyChangedEventHandler handler)
         {
             ThrowExceptionIfDisposed();
-            _bag.Add(propertyName,handler);
+            _bag.Add(propertyName, handler);
         }
 
 
         public void Add(string propertyName, params PropertyChangedEventHandler[] handlers)
         {
             ThrowExceptionIfDisposed();
-            _bag.Add(propertyName,handlers);
+            _bag.Add(propertyName, handlers);
         }
 
         public void Add<T>(Expression<Func<T>> propertyExpression, PropertyChangedEventHandler handler)
         {
             ThrowExceptionIfDisposed();
-            _bag.Add(propertyExpression,handler);
+            _bag.Add(propertyExpression, handler);
         }
 
 
         public void Add<T>(Expression<Func<T>> propertyExpression, params PropertyChangedEventHandler[] handlers)
         {
             ThrowExceptionIfDisposed();
-            _bag.Add(propertyExpression,handlers);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            base.Dispose(disposing);
+            _bag.Add(propertyExpression, handlers);
         }
     }
-
 }
