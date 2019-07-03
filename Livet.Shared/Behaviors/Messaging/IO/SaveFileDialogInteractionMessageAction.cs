38637c00e4994e93a2fec3bbf3e45ab78bad0c9c
terasato <terasato@users.noreply.github.com>
2019-07-03 16:47:25 +0900

    Update SaveFileDialogInteractionMessageAction.cs

--- Livet.Shared/Behaviors/Messaging/IO/SaveFileDialogInteractionMessageAction.cs
+++ Livet.Shared/Behaviors/Messaging/IO/SaveFileDialogInteractionMessageAction.cs
@@ -1,13 +1,12 @@
-﻿using Livet.Messaging.IO;
-using Microsoft.Win32;
-using System.Windows;
-
+﻿using System.Windows;
 using Livet.Messaging;
+using Livet.Messaging.IO;
+using Microsoft.Win32;
 
 namespace Livet.Behaviors.Messaging.IO
 {
     /// <summary>
-    /// 「ファイルを保存する」ダイアログを表示するアクションです。<see cref="SavingFileSelectionMessage"/>に対応します。
+    ///     「ファイルを保存する」ダイアログを表示するアクションです。<see cref="SavingFileSelectionMessage" />に対応します。
     /// </summary>
     public class SaveFileDialogInteractionMessageAction : InteractionMessageAction<DependencyObject>
     {
@@ -16,15 +15,15 @@ namespace Livet.Behaviors.Messaging.IO
             if (message is SavingFileSelectionMessage saveFileMessage)
             {
                 var dialog = new SaveFileDialog
-                                            {
-                                                FileName = saveFileMessage.FileName,
-                                                InitialDirectory = saveFileMessage.InitialDirectory,
-                                                AddExtension = saveFileMessage.AddExtension,
-                                                CreatePrompt = saveFileMessage.CreatePrompt,
-                                                Filter = saveFileMessage.Filter,
-                                                OverwritePrompt = saveFileMessage.OverwritePrompt,
-                                                Title = saveFileMessage.Title
-                                            };
+                {
+                    FileName = saveFileMessage.FileName,
+                    InitialDirectory = saveFileMessage.InitialDirectory,
+                    AddExtension = saveFileMessage.AddExtension,
+                    CreatePrompt = saveFileMessage.CreatePrompt,
+                    Filter = saveFileMessage.Filter,
+                    OverwritePrompt = saveFileMessage.OverwritePrompt,
+                    Title = saveFileMessage.Title
+                };
 
                 saveFileMessage.Response = dialog.ShowDialog().Value ? dialog.FileNames : null;
             }
