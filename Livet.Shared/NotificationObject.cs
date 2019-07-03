964b387c391b8a3642320dd66909e460eede505e
terasato <terasato@users.noreply.github.com>
2019-07-03 16:10:24 +0900

    Update NotificationObject.cs

--- Livet.Shared/NotificationObject.cs
+++ Livet.Shared/NotificationObject.cs
@@ -1,49 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Threading;
+using Livet.Annotations;
 
 namespace Livet
 {
     /// <summary>
-    /// 変更通知オブジェクトの基底クラスです。
+    ///     変更通知オブジェクトの基底クラスです。
     /// </summary>
     [Serializable]
     public class NotificationObject : INotifyPropertyChanged
     {
         /// <summary>
-        /// プロパティ変更通知イベントです。
+        ///     プロパティ変更通知イベントです。
         /// </summary>
         [field: NonSerialized]
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// プロパティ変更通知イベントを発生させます。
+        ///     プロパティ変更通知イベントを発生させます。
         /// </summary>
         /// <param name="source"></param>
         /// <param name="propertyExpression">() => プロパティ形式のラムダ式</param>
         /// <exception cref="NotSupportedException">() => プロパティ 以外の形式のラムダ式が指定されました。</exception>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
-        protected virtual void RaisePropertyChanged<T>(ref T source, Expression<Func<T>> propertyExpression)
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
+        [SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters")]
+        [SuppressMessage("Microsoft.Design", "CA1006:DoNotNestGenericTypesInMemberSignatures")]
+        protected virtual void RaisePropertyChanged<T>(ref T source, [NotNull] Expression<Func<T>> propertyExpression)
         {
             if (propertyExpression == null) throw new ArgumentNullException(nameof(propertyExpression));
 
-            if (!(propertyExpression.Body is MemberExpression)) throw new NotSupportedException("このメソッドでは ()=>プロパティ の形式のラムダ式以外許可されません");
+            if (!(propertyExpression.Body is MemberExpression))
+                throw new NotSupportedException("このメソッドでは ()=>プロパティ の形式のラムダ式以外許可されません");
 
-            var memberExpression = (MemberExpression)propertyExpression.Body;
+            var memberExpression = (MemberExpression) propertyExpression.Body;
             RaisePropertyChanged(memberExpression.Member.Name);
         }
 
         /// <summary>
-        /// プロパティ変更通知イベントを発生させます
+        ///     プロパティ変更通知イベントを発生させます
         /// </summary>
         /// <param name="propertyName">プロパティ名</param>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
-        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged([CallerMemberName] [CanBeNull] string propertyName = "")
         {
             var threadSafeHandler = Interlocked.CompareExchange(ref PropertyChanged, null, null);
             if (threadSafeHandler != null)
@@ -54,7 +58,7 @@ namespace Livet
         }
 
         /// <summary>
-        /// 前と値が違うなら変更して、プロパティ変更通知イベントを発生させます
+        ///     前と値が違うなら変更して、プロパティ変更通知イベントを発生させます
         /// </summary>
         /// <typeparam name="T">プロパティの型</typeparam>
         /// <param name="source">元の値</param>
@@ -62,7 +66,8 @@ namespace Livet
         /// <param name="relatedProperties">このプロパティが変更されたときに PropertyChanged イベントを発行するプロパティの名前の配列</param>
         /// <param name="propertyName">プロパティ名</param>
         /// <returns>値の変更有無</returns>
-        protected bool RaisePropertyChangedIfSet<T>(ref T source, T value, string[] relatedProperties = null, [CallerMemberName]string propertyName = null)
+        protected bool RaisePropertyChangedIfSet<T>(ref T source, T value, string[] relatedProperties = null,
+            [CallerMemberName] string propertyName = null)
         {
             //値が同じだったら何もしない
             if (EqualityComparer<T>.Default.Equals(source, value))
@@ -71,18 +76,14 @@ namespace Livet
             source = value;
             RaisePropertyChanged(propertyName);
             if (relatedProperties != null)
-            {
                 foreach (var p in relatedProperties)
-                {
                     RaisePropertyChanged(p);
-                }
-            }
 
             return true;
         }
 
         /// <summary>
-        /// 前と値が違うなら変更して、プロパティ変更通知イベントを発生させます
+        ///     前と値が違うなら変更して、プロパティ変更通知イベントを発生させます
         /// </summary>
         /// <typeparam name="T">プロパティの型</typeparam>
         /// <param name="source">元の値</param>
@@ -90,7 +91,10 @@ namespace Livet
         /// <param name="relatedProperty">このプロパティが変更されたときに PropertyChanged イベントを発行するプロパティの名前</param>
         /// <param name="propertyName">プロパティ名</param>
         /// <returns>値の変更有無</returns>
-        protected bool RaisePropertyChangedIfSet<T>(ref T source, T value, string relatedProperty, [CallerMemberName]string propertyName = null)
-            => RaisePropertyChangedIfSet(ref source, value, new[] { relatedProperty }, propertyName);
+        protected bool RaisePropertyChangedIfSet<T>(ref T source, T value, string relatedProperty,
+            [CallerMemberName] string propertyName = null)
+        {
+            return RaisePropertyChangedIfSet(ref source, value, new[] {relatedProperty}, propertyName);
+        }
     }
 }
