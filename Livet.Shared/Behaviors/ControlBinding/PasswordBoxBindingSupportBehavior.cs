4fadb01bd2385eddfb4b1d8cb9ae3978689fcd7d
terasato <terasato@users.noreply.github.com>
2019-07-03 16:43:05 +0900

    Update PasswordBoxBindingSupportBehavior.cs

--- Livet.Shared/Behaviors/ControlBinding/PasswordBoxBindingSupportBehavior.cs
+++ Livet.Shared/Behaviors/ControlBinding/PasswordBoxBindingSupportBehavior.cs
@@ -1,54 +1,52 @@
-﻿using System.Windows.Interactivity;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows;
+using System.Windows.Interactivity;
 
 namespace Livet.Behaviors.ControlBinding
 {
     /// <summary>
-    /// PasswordBoxのPasswordをバインド可能にするためのビヘイビアです。
+    ///     PasswordBoxのPasswordをバインド可能にするためのビヘイビアです。
     /// </summary>
     public class PasswordBoxBindingSupportBehavior : Behavior<PasswordBox>
     {
+        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxBindingSupportBehavior),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    SourcePasswordChanged));
+
         /// <summary>
-        /// パスワードを取得、または設定します。
+        ///     パスワードを取得、または設定します。
         /// </summary>
         public string Password
         {
-            get { return (string)GetValue(PasswordProperty); }
+            get { return (string) GetValue(PasswordProperty); }
             set { SetValue(PasswordProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty PasswordProperty =
-            DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxBindingSupportBehavior), new FrameworkPropertyMetadata(string.Empty,FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,SourcePasswordChanged));
-
-        private static void SourcePasswordChanged(DependencyObject sender,DependencyPropertyChangedEventArgs e)
+        private static void SourcePasswordChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            var thisObject = (PasswordBoxBindingSupportBehavior)sender;
-
-            if (thisObject.AssociatedObject == null) return;
-
-            if (thisObject.AssociatedObject.Password != (string)e.NewValue)
+            if (sender is PasswordBoxBindingSupportBehavior thisObject)
             {
-                thisObject.AssociatedObject.Password = (string)e.NewValue;
+                if (thisObject.AssociatedObject == null) return;
+
+                if (thisObject.AssociatedObject.Password != (string) e.NewValue)
+                    thisObject.AssociatedObject.Password = (string) e.NewValue;
             }
         }
 
         private void ControlPasswordChanged(object sender, RoutedEventArgs e)
         {
-            var passwordBox = (PasswordBox)sender;
+            var passwordBox = (PasswordBox) sender;
 
-            if (Password != passwordBox.Password)
-            {
-                Password = passwordBox.Password;
-            }
+            if (Password != passwordBox.Password) Password = passwordBox.Password;
         }
 
         protected override void OnAttached()
         {
             base.OnAttached();
 
-            SourcePasswordChanged(this, new DependencyPropertyChangedEventArgs(PasswordProperty,null,Password));
+            SourcePasswordChanged(this, new DependencyPropertyChangedEventArgs(PasswordProperty, null, Password));
 
             AssociatedObject.PasswordChanged += ControlPasswordChanged;
         }
