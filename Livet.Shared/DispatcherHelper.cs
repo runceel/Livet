94f43a03e6c598fec70a0b152e825138a7d5451a
terasato <terasato@users.noreply.github.com>
2019-07-03 16:25:46 +0900

    Update DispatcherHelper.cs

--- Livet.Shared/DispatcherHelper.cs
+++ Livet.Shared/DispatcherHelper.cs
@@ -1,65 +1,62 @@
 ﻿using System;
-using System.Windows.Threading;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Windows;
+using System.Windows.Threading;
+using Livet.Annotations;
 
 namespace Livet
 {
     /// <summary>
-    /// UIDispatcherへのアクセスを簡易化します。
+    ///     UIDispatcherへのアクセスを簡易化します。
     /// </summary>
     public static class DispatcherHelper
     {
-        private static Dispatcher _uiDispatcher;
+        [CanBeNull] private static Dispatcher _uiDispatcher;
 
-        /// <summary>s
-        /// UIDispatcherを指定、または取得します。通常このプロパティはApp_StartUpで指定されます。
+        /// <summary>
+        ///     UIDispatcherを指定、または取得します。通常このプロパティはApp_StartUpで指定されます。
         /// </summary>
+        [CanBeNull]
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
         public static Dispatcher UIDispatcher
         {
             get
             {
-                if ((bool)(DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue))
-                {
+                if ((bool) DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue)
                     _uiDispatcher = Dispatcher.CurrentDispatcher;
-                }
                 return _uiDispatcher;
             }
-            set
-            {
-                _uiDispatcher = value;
-            }
+            set { _uiDispatcher = value; }
         }
 
         /// <summary>
-        /// UIDispatcherで指定されたアクションを実行します。
+        ///     UIDispatcherで指定されたアクションを実行します。
         /// </summary>
         /// <param name="action">アクション</param>
         /// <exception cref="InvalidOperationException">UIDispatcherインスタンスがApp.StartUpなどで確保されていません。</exception>
         [Obsolete]
-        public static void BeginInvoke(Action action)
+        public static void BeginInvoke([NotNull] Action action)
         {
+            if (action == null) throw new ArgumentNullException(nameof(action));
             if (UIDispatcher == null)
-            {
                 throw new InvalidOperationException("UIDispatcherインスタンスが確保されていません。App.StartUpで確保してください。");
-            }
 
             UIDispatcher.BeginInvoke(action, null);
         }
 
         /// <summary>
-        /// UIDispatcherで指定されたアクションを実行します。
+        ///     UIDispatcherで指定されたアクションを実行します。
         /// </summary>
         /// <param name="action">アクション</param>
         /// <param name="priority">DispatcherPriority</param>
         /// <exception cref="InvalidOperationException">UIDispatcherインスタンスがApp.StartUpなどで確保されていません。</exception>
         [Obsolete]
-        public static void BeginInvoke(Action action, DispatcherPriority priority)
+        public static void BeginInvoke([NotNull] Action action, DispatcherPriority priority)
         {
+            if (action == null) throw new ArgumentNullException(nameof(action));
             if (UIDispatcher == null)
-            {
                 throw new InvalidOperationException("UIDispatcherインスタンスが確保されていません。App.StartUpで確保してください。");
-            }
 
             UIDispatcher.BeginInvoke(action, priority, null);
         }
