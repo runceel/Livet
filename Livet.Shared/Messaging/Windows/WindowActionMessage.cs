e909f5a5ba64e8c7cf9c3d403d5c90c3bb85a97f
terasato <terasato@users.noreply.github.com>
2019-07-03 22:54:56 +0900

    Update WindowActionMessage.cs

--- Livet.Shared/Messaging/Windows/WindowActionMessage.cs
+++ Livet.Shared/Messaging/Windows/WindowActionMessage.cs
@@ -1,64 +1,68 @@
 ﻿using System.Windows;
+using Livet.Annotations;
 
 namespace Livet.Messaging.Windows
 {
     /// <summary>
-    /// Windowを最大化・最小化・閉じる・通常化させるための相互作用メッセージです。
+    ///     Windowを最大化・最小化・閉じる・通常化させるための相互作用メッセージです。
     /// </summary>
     public class WindowActionMessage : InteractionMessage
     {
+        // Using a DependencyProperty as the backing store for Action.  This enables animation, styling, binding, etc...
+        [NotNull] public static readonly DependencyProperty ActionProperty =
+            DependencyProperty.Register("Action", typeof(WindowAction), typeof(WindowActionMessage),
+                new PropertyMetadata());
+
         public WindowActionMessage()
         {
         }
 
         /// <summary>
-        /// メッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     メッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="messageKey">メッセージキー</param>
         public WindowActionMessage(string messageKey)
-            : base(messageKey) { }
+            : base(messageKey)
+        {
+        }
 
         /// <summary>
-        /// メッセージキーとWindowが遷移すべき状態を定義して、新しい相互作用メッセージのインスタンスを生成します。
+        ///     メッセージキーとWindowが遷移すべき状態を定義して、新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="action">Windowが遷移すべき状態を表すWindowAction列挙体</param>
         /// <param name="messageKey">メッセージキー</param>
-        public WindowActionMessage(WindowAction action,string messageKey)
-            :this(messageKey)
+        public WindowActionMessage(WindowAction action, string messageKey)
+            : this(messageKey)
         {
             Action = action;
         }
 
         /// <summary>
-        /// Windowが遷移すべき状態を定義して、新しい相互作用メッセージのインスタンスを生成します。
+        ///     Windowが遷移すべき状態を定義して、新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="action">Windowが遷移すべき状態を表すWindowAction列挙体</param>
         public WindowActionMessage(WindowAction action)
-            : this(action,null) { }
+            : this(action, null)
+        {
+        }
 
         /// <summary>
-        /// 派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br/>
-        /// 通常このメソッドは、自身の新しいインスタンスを返すように実装します。
+        ///     Windowが遷移すべき状態を表すWindowAction列挙体を指定、または取得します。
+        /// </summary>
+        public WindowAction Action
+        {
+            get { return (WindowAction) GetValue(ActionProperty); }
+            set { SetValue(ActionProperty, value); }
+        }
+
+        /// <summary>
+        ///     派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br />
+        ///     通常このメソッドは、自身の新しいインスタンスを返すように実装します。
         /// </summary>
         /// <returns>自身の新しいインスタンス</returns>
         protected override Freezable CreateInstanceCore()
         {
             return new WindowActionMessage(MessageKey);
         }
-
-        /// <summary>
-        /// Windowが遷移すべき状態を表すWindowAction列挙体を指定、または取得します。
-        /// </summary>
-        public WindowAction Action
-        {
-            get { return (WindowAction)GetValue(ActionProperty); }
-            set { SetValue(ActionProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for Action.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ActionProperty =
-            DependencyProperty.Register("Action", typeof(WindowAction), typeof(WindowActionMessage), new PropertyMetadata());
-
-        
     }
 }
