2f25fd95557318fbde1d19871e4e8abee28ea7b5
terasato <terasato@users.noreply.github.com>
2019-07-03 22:56:12 +0900

    Update TransitionMessage.cs

--- Livet.Shared/Messaging/TransitionMessage.cs
+++ Livet.Shared/Messaging/TransitionMessage.cs
@@ -1,141 +1,153 @@
-﻿using System.Windows;
-using System;
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+using Livet.Annotations;
 
 namespace Livet.Messaging
 {
     /// <summary>
-    /// 画面遷移アクション用の相互作用メッセージです。
+    ///     画面遷移アクション用の相互作用メッセージです。
     /// </summary>
-    [System.Windows.Markup.ContentProperty("TransitionViewModel")]
+    [ContentProperty("TransitionViewModel")]
     public class TransitionMessage : ResponsiveInteractionMessage<bool?>
     {
+        // Using a DependencyProperty as the backing store for TransitionViewModel.  This enables animation, styling, binding, etc...
+        [NotNull] public static readonly DependencyProperty TransitionViewModelProperty =
+            DependencyProperty.Register("TransitionViewModel", typeof(ViewModel), typeof(TransitionMessage),
+                new PropertyMetadata(null));
+
+        // Using a DependencyProperty as the backing store for Mode.  This enables animation, styling, binding, etc...
+        [NotNull] public static readonly DependencyProperty ModeProperty =
+            DependencyProperty.Register("Mode", typeof(TransitionMode), typeof(TransitionMessage),
+                new PropertyMetadata(TransitionMode.UnKnown));
+
+        // Using a DependencyProperty as the backing store for WindowType.  This enables animation, styling, binding, etc...
+        [NotNull] public static readonly DependencyProperty WindowTypeProperty =
+            DependencyProperty.Register("WindowType", typeof(Type), typeof(TransitionMessage),
+                new PropertyMetadata(null));
+
         public TransitionMessage()
         {
         }
 
         /// <summary>
-        /// メッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     メッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="messageKey">メッセージキー</param>
-        public TransitionMessage(string messageKey) : base(messageKey) { }
+        public TransitionMessage(string messageKey) : base(messageKey)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowのDataContextに設定するViewModelとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowのDataContextに設定するViewModelとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         /// <param name="messageKey">メッセージキー</param>
         public TransitionMessage(ViewModel transitionViewModel, string messageKey)
-            : this(null, transitionViewModel, TransitionMode.UnKnown, messageKey) { }
+            : this(null, transitionViewModel, TransitionMode.UnKnown, messageKey)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowのDataContextに設定するViewModelと画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowのDataContextに設定するViewModelと画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         /// <param name="mode">画面遷移の方法を決定するTransitionMode列挙体。初期値はUnKnownです。</param>
         /// <param name="messageKey">メッセージキー</param>
         public TransitionMessage(ViewModel transitionViewModel, TransitionMode mode, string messageKey)
-            : this(null, transitionViewModel, mode, messageKey) { }
+            : this(null, transitionViewModel, mode, messageKey)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowの型と新しいWindowに設定するViewModel、画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowの型と新しいWindowに設定するViewModel、画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="windowType">新しいWindowの型</param>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         /// <param name="mode">画面遷移の方法を決定するTransitionMode列挙体。初期値はUnKnownです。</param>
         /// <param name="messageKey">メッセージキー</param>
         public TransitionMessage(Type windowType, ViewModel transitionViewModel, TransitionMode mode, string messageKey)
-            :base(messageKey)
+            : base(messageKey)
         {
             Mode = mode;
             TransitionViewModel = transitionViewModel;
 
             if (windowType != null)
-            {
                 if (!windowType.IsSubclassOf(typeof(Window)))
-                {
                     throw new ArgumentException("Windowの派生クラスを指定してください。", nameof(windowType));
-                }
-            }
 
             WindowType = windowType;
         }
 
         /// <summary>
-        /// 新しいWindowのDataContextに設定するViewModelを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowのDataContextに設定するViewModelを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         public TransitionMessage(ViewModel transitionViewModel)
-            : this(null, transitionViewModel, TransitionMode.UnKnown, null) { }
+            : this(null, transitionViewModel, TransitionMode.UnKnown, null)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowのDataContextに設定するViewModelと画面遷移モードを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowのDataContextに設定するViewModelと画面遷移モードを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         /// <param name="mode">画面遷移の方法を決定するTransitionMode列挙体。初期値はUnKnownです。</param>
         public TransitionMessage(ViewModel transitionViewModel, TransitionMode mode)
-            : this(null, transitionViewModel, mode, null) { }
+            : this(null, transitionViewModel, mode, null)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowの型と新しいWindowに設定するViewModel、画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
+        ///     新しいWindowの型と新しいWindowに設定するViewModel、画面遷移モードとメッセージキーを指定して新しい相互作用メッセージのインスタンスを生成します。
         /// </summary>
         /// <param name="windowType">新しいWindowの型</param>
         /// <param name="transitionViewModel">新しいWindowのDataContextに設定するViewModel</param>
         /// <param name="mode">画面遷移の方法を決定するTransitionMode列挙体。初期値はUnKnownです。</param>
         public TransitionMessage(Type windowType, ViewModel transitionViewModel, TransitionMode mode)
-            : this(windowType,transitionViewModel,mode,null) { }
+            : this(windowType, transitionViewModel, mode, null)
+        {
+        }
 
         /// <summary>
-        /// 新しいWindowのDataContextに設定するViewModelを指定、または取得します。
+        ///     新しいWindowのDataContextに設定するViewModelを指定、または取得します。
         /// </summary>
         public ViewModel TransitionViewModel
         {
-            get { return (ViewModel)GetValue(TransitionViewModelProperty); }
+            get { return (ViewModel) GetValue(TransitionViewModelProperty); }
             set { SetValue(TransitionViewModelProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for TransitionViewModel.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TransitionViewModelProperty =
-            DependencyProperty.Register("TransitionViewModel", typeof(ViewModel), typeof(TransitionMessage), new PropertyMetadata(null));
-
 
         /// <summary>
-        /// 新しいWindowの表示方法を決定するTransitionModeを指定、または取得します。<br/>
-        /// 初期値はUnKnownです。
+        ///     新しいWindowの表示方法を決定するTransitionModeを指定、または取得します。<br />
+        ///     初期値はUnKnownです。
         /// </summary>
         public TransitionMode Mode
         {
-            get { return (TransitionMode)GetValue(ModeProperty); }
+            get { return (TransitionMode) GetValue(ModeProperty); }
             set { SetValue(ModeProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Mode.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ModeProperty =
-            DependencyProperty.Register("Mode", typeof(TransitionMode), typeof(TransitionMessage), new PropertyMetadata(TransitionMode.UnKnown));
-
 
         /// <summary>
-        /// 新しいWindowの型を指定、または取得します。
+        ///     新しいWindowの型を指定、または取得します。
         /// </summary>
         public Type WindowType
         {
-            get { return (Type)GetValue(WindowTypeProperty); }
+            get { return (Type) GetValue(WindowTypeProperty); }
             set { SetValue(WindowTypeProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for WindowType.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty WindowTypeProperty =
-            DependencyProperty.Register("WindowType", typeof(Type), typeof(TransitionMessage), new PropertyMetadata(null));
-
-        
 
         /// <summary>
-        /// 派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br/>
-        /// 通常このメソッドは、自身の新しいインスタンスを返すように実装します。
+        ///     派生クラスでは必ずオーバーライドしてください。Freezableオブジェクトとして必要な実装です。<br />
+        ///     通常このメソッドは、自身の新しいインスタンスを返すように実装します。
         /// </summary>
         /// <returns>自身の新しいインスタンス</returns>
         protected override Freezable CreateInstanceCore()
         {
-            return new TransitionMessage(TransitionViewModel,MessageKey);
+            return new TransitionMessage(TransitionViewModel, MessageKey);
         }
     }
 }
