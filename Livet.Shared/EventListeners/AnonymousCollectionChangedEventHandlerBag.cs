fad06643b4c2fd9864a1fe9d5a4459ea6f5bf9ff
terasato <terasato@users.noreply.github.com>
2019-07-03 17:03:25 +0900

    Update AnonymousCollectionChangedEventHandlerBag.cs

--- Livet.Shared/EventListeners/AnonymousCollectionChangedEventHandlerBag.cs
+++ Livet.Shared/EventListeners/AnonymousCollectionChangedEventHandlerBag.cs
@@ -1,21 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using Livet.Annotations;
 
 namespace Livet.EventListeners
 {
-    internal class AnonymousCollectionChangedEventHandlerBag : IEnumerable<KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>>
+    internal class AnonymousCollectionChangedEventHandlerBag : IEnumerable<
+        KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>>
     {
-        private Dictionary<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>> _handlerDictionary = new Dictionary<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>();
-        private WeakReference<INotifyCollectionChanged> _source;
+        [NotNull] private readonly List<NotifyCollectionChangedEventHandler> _allHandlerList =
+            new List<NotifyCollectionChangedEventHandler>();
 
-        private List<NotifyCollectionChangedEventHandler> _allHandlerList = new List<NotifyCollectionChangedEventHandler>();
-        
-        private Dictionary<List<NotifyCollectionChangedEventHandler>,object> _lockObjectDictionary = new Dictionary<List<NotifyCollectionChangedEventHandler>, object>();
+        [NotNull] private readonly object _allHandlerListLockObject = new object();
 
-        private object _handlerDictionaryLockObject = new object();
-        private object _allHandlerListLockObject = new object();
+        [NotNull] private readonly Dictionary<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>
+            _handlerDictionary =
+                new Dictionary<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>();
+
+        [NotNull] private readonly object _handlerDictionaryLockObject = new object();
+
+        [NotNull] private readonly Dictionary<List<NotifyCollectionChangedEventHandler>, object> _lockObjectDictionary =
+            new Dictionary<List<NotifyCollectionChangedEventHandler>, object>();
+
+        [NotNull] private readonly WeakReference<INotifyCollectionChanged> _source;
 
         internal AnonymousCollectionChangedEventHandlerBag(INotifyCollectionChanged source)
         {
@@ -23,10 +32,23 @@ namespace Livet.EventListeners
             _source = new WeakReference<INotifyCollectionChanged>(source);
         }
 
-        internal AnonymousCollectionChangedEventHandlerBag(INotifyCollectionChanged source,NotifyCollectionChangedEventHandler handler) : this(source)
+        internal AnonymousCollectionChangedEventHandlerBag(INotifyCollectionChanged source,
+            NotifyCollectionChangedEventHandler handler) : this(source)
         {
             if (handler == null) throw new ArgumentNullException(nameof(handler));
             RegisterHandler(handler);
+        }
+
+        IEnumerator<KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>>
+            IEnumerable<KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>>.
+            GetEnumerator()
+        {
+            return _handlerDictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _handlerDictionary.GetEnumerator();
         }
 
         internal void RegisterHandler(NotifyCollectionChangedEventHandler handler)
@@ -45,17 +67,19 @@ namespace Livet.EventListeners
                 if (!_handlerDictionary.TryGetValue(action, out bag))
                 {
                     bag = new List<NotifyCollectionChangedEventHandler>();
-                    _lockObjectDictionary.Add(bag,new object());
+                    _lockObjectDictionary.Add(bag, new object());
                     _handlerDictionary[action] = bag;
                 }
+
                 bag.Add(handler);
             }
         }
 
-        internal void ExecuteHandler(NotifyCollectionChangedEventArgs e)
+        internal void ExecuteHandler([NotNull] NotifyCollectionChangedEventArgs e)
         {
-            INotifyCollectionChanged sourceResult;
-            var result = _source.TryGetTarget(out sourceResult);
+            if (e == null) throw new ArgumentNullException(nameof(e));
+
+            var result = _source.TryGetTarget(out var sourceResult);
 
             if (!result) return;
 
@@ -64,37 +88,19 @@ namespace Livet.EventListeners
             {
                 _handlerDictionary.TryGetValue(e.Action, out list);
             }
+
             if (list != null)
-            {
                 lock (_lockObjectDictionary[list])
                 {
-                    foreach (var handler in list)
-                    {
-                        handler(sourceResult, e);
-                    }
+                    foreach (var handler in list) handler(sourceResult, e);
                 }
-            }
 
             lock (_allHandlerListLockObject)
             {
                 if (_allHandlerList.Any())
-                {
                     foreach (var handler in _allHandlerList)
-                    {
                         handler(sourceResult, e);
-                    }
-                }
             }
-        }
-
-        IEnumerator<KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>> IEnumerable<KeyValuePair<NotifyCollectionChangedAction, List<NotifyCollectionChangedEventHandler>>>.GetEnumerator()
-        {
-            return _handlerDictionary.GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return _handlerDictionary.GetEnumerator();
         }
 
         internal void Add(NotifyCollectionChangedEventHandler handler)
@@ -108,12 +114,11 @@ namespace Livet.EventListeners
         }
 
 
-        internal void Add(NotifyCollectionChangedAction action, params NotifyCollectionChangedEventHandler[] handlers)
+        internal void Add(NotifyCollectionChangedAction action, [NotNull] params NotifyCollectionChangedEventHandler[] handlers)
         {
-            foreach (var handler in handlers)
-            {
-                RegisterHandler(action, handler);
-            }
+            if (handlers == null) throw new ArgumentNullException(nameof(handlers));
+
+            foreach (var handler in handlers) RegisterHandler(action, handler);
         }
     }
 }
