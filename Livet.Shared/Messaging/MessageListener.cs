7fc450aaa1e8da736d57b52144a2ed7d6a745efc
terasato <terasato@users.noreply.github.com>
2019-07-03 23:02:42 +0900

    Update MessageListener.cs

--- Livet.Shared/Messaging/MessageListener.cs
+++ Livet.Shared/Messaging/MessageListener.cs
@@ -1,24 +1,37 @@
-﻿using System.Collections.Concurrent;
-using System.Windows.Threading;
-using Livet.EventListeners.WeakEvents;
-using System;
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Windows.Threading;
+using Livet.Annotations;
+using Livet.EventListeners.WeakEvents;
 
 namespace Livet.Messaging
 {
-    public sealed class MessageListener : IDisposable, IEnumerable<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>>
+    public sealed class MessageListener : IDisposable,
+        IEnumerable<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>>
     {
-        private LivetWeakEventListener<EventHandler<InteractionMessageRaisedEventArgs>, InteractionMessageRaisedEventArgs> _listener;
-        private WeakReference<InteractionMessenger> _source;
-        private ConcurrentDictionary<string, ConcurrentBag<Action<InteractionMessage>>> _actionDictionary = new ConcurrentDictionary<string, ConcurrentBag<Action<InteractionMessage>>>();
+        [NotNull]
+        private readonly ConcurrentDictionary<string, ConcurrentBag<Action<InteractionMessage>>> _actionDictionary =
+            new ConcurrentDictionary<string, ConcurrentBag<Action<InteractionMessage>>>();
+
+        [NotNull] private readonly LivetWeakEventListener<EventHandler<InteractionMessageRaisedEventArgs>,
+            InteractionMessageRaisedEventArgs> _listener;
+
+        [NotNull] private readonly WeakReference<InteractionMessenger> _source;
+        [NotNull] private Dispatcher _dispatcher;
 
         private bool _disposed;
 
-        public MessageListener(InteractionMessenger messenger)
+        public MessageListener([NotNull] InteractionMessenger messenger)
         {
-            Dispatcher = Dispatcher.CurrentDispatcher;
+            if (messenger == null) throw new ArgumentNullException(nameof(messenger));
+
+            _dispatcher = Dispatcher.CurrentDispatcher;
             _source = new WeakReference<InteractionMessenger>(messenger);
-            _listener = new LivetWeakEventListener<EventHandler<InteractionMessageRaisedEventArgs>, InteractionMessageRaisedEventArgs>
+            _listener =
+                new LivetWeakEventListener<EventHandler<InteractionMessageRaisedEventArgs>,
+                    InteractionMessageRaisedEventArgs>
                 (
                     h => h,
                     h => messenger.Raised += h,
@@ -27,20 +40,44 @@ namespace Livet.Messaging
                 );
         }
 
-        public MessageListener(InteractionMessenger messenger, string messageKey, Action<InteractionMessage> action)
+        public MessageListener([NotNull] InteractionMessenger messenger, [CanBeNull] string messageKey,
+            Action<InteractionMessage> action)
             : this(messenger)
         {
-            if (messageKey == null)
-            {
-                messageKey = string.Empty;
-            }
+            if (messageKey == null) messageKey = string.Empty;
 
             RegisterAction(messageKey, action);
         }
 
-        public MessageListener(InteractionMessenger messenger, Action<InteractionMessage> action)
+        public MessageListener([NotNull] InteractionMessenger messenger, Action<InteractionMessage> action)
             : this(messenger, null, action)
         {
+        }
+
+        [NotNull]
+        public Dispatcher Dispatcher
+        {
+            get { return _dispatcher; }
+            set { _dispatcher = value ?? throw new ArgumentNullException(nameof(value)); }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        IEnumerator<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>>
+            IEnumerable<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>>.GetEnumerator()
+        {
+            ThrowExceptionIfDisposed();
+            return _actionDictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            ThrowExceptionIfDisposed();
+            return _actionDictionary.GetEnumerator();
         }
 
         public void RegisterAction(Action<InteractionMessage> action)
@@ -61,82 +98,50 @@ namespace Livet.Messaging
 
             var message = e.Message;
 
-            var cloneMessage = (InteractionMessage)message.Clone();
+            var cloneMessage = (InteractionMessage) message.Clone();
 
             cloneMessage.Freeze();
 
-            DoActionOnDispatcher(() =>
-                {
-                    GetValue(e, cloneMessage);
-                });
+            DoActionOnDispatcher(() => { GetValue(e, cloneMessage); });
 
             var responsiveMessage = message as ResponsiveInteractionMessage;
 
             object response;
             if (responsiveMessage != null &&
-                (response = ((ResponsiveInteractionMessage)cloneMessage).Response) != null)
-            {
+                (response = ((ResponsiveInteractionMessage) cloneMessage).Response) != null)
                 responsiveMessage.Response = response;
-            }
         }
 
         private void GetValue(InteractionMessageRaisedEventArgs e, InteractionMessage cloneMessage)
         {
-            InteractionMessenger sourceResult;
-            var result = _source.TryGetTarget(out sourceResult);
+            var result = _source.TryGetTarget(out _);
 
             if (!result) return;
 
             if (e.Message.MessageKey != null)
             {
-                ConcurrentBag<Action<InteractionMessage>> list;
-                _actionDictionary.TryGetValue(e.Message.MessageKey, out list);
+                _actionDictionary.TryGetValue(e.Message.MessageKey, out var list);
 
                 if (list != null)
-                {
                     foreach (var action in list)
-                    {
                         action(cloneMessage);
-                    }
-                }
             }
 
-            ConcurrentBag<Action<InteractionMessage>> allList;
-            _actionDictionary.TryGetValue(string.Empty, out allList);
+            _actionDictionary.TryGetValue(string.Empty, out var allList);
             if (allList != null)
-            {
                 foreach (var action in allList)
-                {
                     action(cloneMessage);
-                }
-            }
         }
 
-        private void DoActionOnDispatcher(Action action)
+        private void DoActionOnDispatcher([NotNull] Action action)
         {
+            if (action == null) throw new ArgumentNullException(nameof(action));
+
             if (Dispatcher.CheckAccess())
-            {
                 action();
-            }
             else
-            {
                 Dispatcher.Invoke(action);
-            }
         }
-
-        IEnumerator<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>> IEnumerable<KeyValuePair<string, ConcurrentBag<Action<InteractionMessage>>>>.GetEnumerator()
-        {
-            ThrowExceptionIfDisposed();
-            return _actionDictionary.GetEnumerator();
-        }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            ThrowExceptionIfDisposed();
-            return _actionDictionary.GetEnumerator();
-        }
-
-        public Dispatcher Dispatcher { get; set; }
 
         public void Add(Action<InteractionMessage> action)
         {
@@ -149,36 +154,23 @@ namespace Livet.Messaging
         }
 
 
-        public void Add(string messageKey, params Action<InteractionMessage>[] actions)
+        public void Add(string messageKey, [NotNull] params Action<InteractionMessage>[] actions)
         {
-            foreach (var action in actions)
-            {
-                RegisterAction(messageKey, action);
-            }
+            if (actions == null) throw new ArgumentNullException(nameof(actions));
+
+            foreach (var action in actions) RegisterAction(messageKey, action);
         }
 
         private void ThrowExceptionIfDisposed()
         {
-            if (_disposed)
-            {
-                throw new ObjectDisposedException("EventListener");
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
+            if (_disposed) throw new ObjectDisposedException("EventListener");
         }
 
         private void Dispose(bool disposing)
         {
             if (_disposed) return;
 
-            if (disposing)
-            {
-                _listener.Dispose();
-            }
+            if (disposing) _listener.Dispose();
             _disposed = true;
         }
     }
