1157de1841fea67b6d0ca890060f75d7c478a88e
terasato <terasato@users.noreply.github.com>
2019-07-03 16:52:17 +0900

    Update ListenerCommand.cs

--- Livet.Shared/Commands/ListenerCommand.cs
+++ Livet.Shared/Commands/ListenerCommand.cs
@@ -1,63 +1,54 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Windows.Input;
+using Livet.Annotations;
 
 namespace Livet.Commands
 {
     /// <summary>
-    /// ViewModelにおいて、Viewからメッセージ、あるいはオブジェクトを受け取って動作するコマンドを表します。
+    ///     ViewModelにおいて、Viewからメッセージ、あるいはオブジェクトを受け取って動作するコマンドを表します。
     /// </summary>
     /// <typeparam name="T">Viewから受け取るオブジェクトの型</typeparam>
-    public sealed class ListenerCommand<T> :Command,ICommand,INotifyPropertyChanged
+    public sealed class ListenerCommand<T> : Command, ICommand, INotifyPropertyChanged
     {
-        Action<T> _execute;
-        Func<bool> _canExecute;
+        [CanBeNull] private readonly Func<bool> _canExecute;
+        [NotNull] private readonly Action<T> _execute;
 
         /// <summary>
-        /// コンストラクタ
+        ///     コンストラクタ
         /// </summary>
         /// <param name="execute">コマンドが実行するAction</param>
-        public ListenerCommand(Action<T> execute) : this(execute, null) { }
+        public ListenerCommand([NotNull] Action<T> execute) : this(execute, null)
+        {
+        }
 
         /// <summary>
-        /// コンストラクタ
+        ///     コンストラクタ
         /// </summary>
         /// <param name="execute">コマンドが実行するAction</param>
         /// <param name="canExecute">コマンドが実行可能かどうかをあらわすFunc&lt;bool&gt;</param>
-        public ListenerCommand(Action<T> execute, Func<bool> canExecute)
+        public ListenerCommand([NotNull] Action<T> execute, [CanBeNull] Func<bool> canExecute)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
         /// <summary>
-        /// コマンドが実行可能かどうかを取得します。
+        ///     コマンドが実行可能かどうかを取得します。
         /// </summary>
         public bool CanExecute
         {
             get { return _canExecute == null || _canExecute(); }
         }
 
-        /// <summary>
-        /// コマンドを実行します。
-        /// </summary>
-        /// <param name="parameter">Viewから渡されたオブジェクト</param>
-        public void Execute(T parameter)
-        {
-            _execute(parameter);
-        }
-
         void ICommand.Execute(object parameter)
         {
             if (parameter == null)
-            {
-                Execute(default(T));
-            }
+                Execute(default);
             else
-            {
-                Execute((T)parameter);
-            }
+                Execute((T) parameter);
         }
 
         bool ICommand.CanExecute(object parameter)
@@ -66,25 +57,34 @@ namespace Livet.Commands
         }
 
         /// <summary>
-        /// コマンドが実行可能かどうかが変化した時に発生します。
+        ///     コマンドが実行可能かどうかが変化した時に発生します。
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        ///     コマンドを実行します。
+        /// </summary>
+        /// <param name="parameter">Viewから渡されたオブジェクト</param>
+        public void Execute(T parameter)
+        {
+            _execute(parameter);
+        }
+
+        [NotifyPropertyChangedInvocator]
         private void OnPropertyChanged()
         {
             var handler = Interlocked.CompareExchange(ref PropertyChanged, null, null);
-            handler?.Invoke(this,EventArgsFactory.GetPropertyChangedEventArgs("CanExecute"));
+            handler?.Invoke(this, EventArgsFactory.GetPropertyChangedEventArgs("CanExecute"));
         }
 
         /// <summary>
-        /// コマンドが実行可能かどうかが変化したことを通知します。
+        ///     コマンドが実行可能かどうかが変化したことを通知します。
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
+        [SuppressMessage("Microsoft.Design", "CA1030:UseEventsWhereAppropriate")]
         public void RaiseCanExecuteChanged()
         {
             OnPropertyChanged();
             OnCanExecuteChanged();
         }
-
     }
 }
