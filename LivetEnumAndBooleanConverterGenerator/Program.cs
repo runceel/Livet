a475646ae0718fde4fd6ef3918e14ae47de55cdf
terasato <terasato@users.noreply.github.com>
2019-07-23 14:49:30 +0900

    Update Program.cs

--- LivetEnumAndBooleanConverterGenerator/Program.cs
+++ LivetEnumAndBooleanConverterGenerator/Program.cs
@@ -9,6 +9,7 @@ namespace LivetEnumAndBooleanConverterGenerator
 {
     internal class Program
     {
+        // ReSharper disable once UnusedParameter.Local
         private static void Main(string[] args)
         {
             var assemblies =
