714867f3189aedfb1522c1621be8f7bb82366ef3
Kazuki Ota <kaota@microsoft.com>
2019-09-28 10:42:00 +0900

    migrate multi target frameworks project

---
+++ Livet.Test/Commands/CommandTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Livet.Commands;
+using System.Windows.Input;
+using System.Threading;
+
+namespace Livet.NUnit.Commands
+{
+    [TestFixture()]
+    public class CommandTest
+    {
+        [Test()]
+        [Timeout(3000)]
+        public void CanExecuteHandlerBasicTest()
+        {
+            using (var dispatcher = new TestDispatcherContext())
+            {
+                DispatcherHelper.UIDispatcher = dispatcher.Dispatcher;
+
+                var handlerResultList = new List<string>();
+
+                var command = new ViewModelCommand(() => { }, () => true);
+
+                EventHandler handler1 = (sender, e) => handlerResultList.Add("Handler1");
+                EventHandler handler2 = (sender, e) => handlerResultList.Add("Handler2");
+                EventHandler handler3 = (sender, e) => handlerResultList.Add("Handler3");
+
+                var semaphore = new Semaphore(0, 1);
+                EventHandler releaseHandler = (sender, e) => semaphore.Release();
+
+                command.CanExecuteChanged += handler1;
+                command.CanExecuteChanged += handler2;
+                command.CanExecuteChanged += handler3;
+                command.CanExecuteChanged += releaseHandler;
+
+                command.RaiseCanExecuteChanged();
+                semaphore.WaitOne(1000).Is(true);
+
+                handlerResultList.Count.Is(3);
+                handlerResultList.Is("Handler1", "Handler2", "Handler3");
+
+                handlerResultList.Clear();
+
+                command.CanExecuteChanged -= handler2;
+
+                command.RaiseCanExecuteChanged();
+                semaphore.WaitOne(1000).Is(true);
+
+                handlerResultList.Count.Is(2);
+                handlerResultList.Is("Handler1", "Handler3");
+            }
+        }
+
+        [Test()]
+        [Timeout(3000)]
+        public void WeakHandlerTest()
+        {
+#if DEBUG
+            Assert.Warn("This test case should run on release build.");
+            return;
+#endif
+            using (var dispatcher = new TestDispatcherContext())
+            {
+                int eventReceivedCount = 0;
+
+                DispatcherHelper.UIDispatcher = dispatcher.Dispatcher;
+
+                var handlerResultList = new List<string>();
+
+                var command = new ViewModelCommand(() => { }, () => true);
+
+                var realReceiver1 = new EventReceiver(command, () => eventReceivedCount++);
+                var realReceiver2 = new EventReceiver(command, () => eventReceivedCount++);
+
+                var receiver1 = new WeakReference(realReceiver1);
+                var receiver2 = new WeakReference(realReceiver2);
+                var receiver3 = new EventReceiver(command, () => eventReceivedCount++);
+
+                var semaphore = new Semaphore(0, 1);
+                var releaseReceiver = new EventReceiver(command, () => semaphore.Release());
+
+                command.RaiseCanExecuteChanged();
+                semaphore.WaitOne(1000).Is(true);
+
+                eventReceivedCount.Is(3);
+
+                realReceiver1 = null;
+                realReceiver2 = null;
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                receiver1.IsAlive.Is(false);
+                receiver2.IsAlive.Is(false);
+
+                receiver3.IsNot(null); // do not remove this line. this line is for no released receiver3 by GC.
+                releaseReceiver.IsNot(null); // do not remove this line. this line is for no released releaseReceiver by GC.
+
+                command.RaiseCanExecuteChanged();
+                semaphore.WaitOne(1000).Is(true);
+                eventReceivedCount.Is(4);
+            }
+        }
+
+        public class EventReceiver
+        {
+            public EventReceiver(ICommand command,Action eventReceivedAction)
+            {
+                HandlerStrongReference = new EventHandler((sender,e) => eventReceivedAction());
+                command.CanExecuteChanged += HandlerStrongReference;
+            }
+
+            public EventHandler HandlerStrongReference
+            {
+                get;
+                private set;
+            }
+
+        }
+    }
+}
---
+++ LivetCask.Extensions/Dialogs/CommonOpenFileFolderSelectionDialog.cs
@@ -0,0 +1,113 @@
+using System;
+using System.IO;
+using System.Windows;
+using Livet.Annotations;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Livet.Dialogs
+{
+    /// <summary>
+    ///     <see cref="FolderSelectionDialog" /> implementation based on Win32 Common Item Dialog.
+    /// </summary>
+    internal sealed class CommonOpenFileFolderSelectionDialog : FolderSelectionDialog
+    {
+        [NotNull] private readonly CommonOpenFileDialog _commonOpenFileDialog;
+        [CanBeNull] private readonly string _defaultTitle;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CommonOpenFileFolderSelectionDialog" /> class.
+        /// </summary>
+        public CommonOpenFileFolderSelectionDialog()
+        {
+            _commonOpenFileDialog = new CommonOpenFileDialog {IsFolderPicker = true, Multiselect = false};
+            _defaultTitle = _commonOpenFileDialog.Title;
+        }
+
+        /// <summary>
+        ///     Gets or sets the title of the dialog.
+        /// </summary>
+        /// <value>
+        ///     The title of the dialog.
+        ///     Some dialog cannot support this value.
+        ///     The <c>null</c> or empty indicates using default title.
+        /// </value>
+        public override string Title
+        {
+            get { return _commonOpenFileDialog.Title; }
+            set { _commonOpenFileDialog.Title = string.IsNullOrEmpty(value) ? _defaultTitle : value; }
+        }
+
+        /// <summary>
+        ///     This property is not supported.
+        /// </summary>
+        /// <value>Always <see cref="F:String.Empty" />.</value>
+        public override string Description
+        {
+            get { return string.Empty; }
+            set { }
+        }
+
+        /// <summary>
+        ///     Gets or sets the selected path.
+        /// </summary>
+        /// <value>
+        ///     The selected path. This will be default path when the dialog is opened.
+        /// </value>
+        public override string SelectedPath
+        {
+            get { return _commonOpenFileDialog.FileName; }
+            set
+            {
+                DirectoryInfo asDirectory = null;
+                try { asDirectory = value != null ? new DirectoryInfo(value) : null; }
+                catch (ArgumentException) { }
+
+                if (asDirectory == null)
+                    _commonOpenFileDialog.DefaultFileName = value;
+                else
+                {
+                    //// Set parent.
+                    _commonOpenFileDialog.DefaultFileName = asDirectory.Name;
+
+                    //// Set "My Computer", if drive root
+                    _commonOpenFileDialog.InitialDirectory = asDirectory.Parent?.FullName
+                                                             ?? "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing">
+        ///     <c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only
+        ///     unmanaged resources.
+        /// </param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _commonOpenFileDialog.Dispose();
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        ///     Shows the dialog with specified <see cref="Window" /> as host window.
+        /// </summary>
+        /// <param name="hostWindow">The host window which will host this dialog. This value will not be <c>null</c>.</param>
+        /// <returns>
+        ///     The result of the dialog.
+        /// </returns>
+        protected override bool? ShowDialogCore([NotNull] Window hostWindow)
+        {
+            if (hostWindow == null) throw new ArgumentNullException(nameof(hostWindow));
+
+            // ReSharper disable once SwitchStatementMissingSomeCases
+            switch (_commonOpenFileDialog.ShowDialog(hostWindow))
+            {
+                case CommonFileDialogResult.Ok:
+                    return true;
+                default:
+                    return null;
+            }
+        }
+    }
+}
