ba27039cb2dd062ccfe179db868d1fd81cf561f6
Kazuki Ota <kaota@microsoft.com>
2019-09-29 16:41:09 +0900

    for v3.2.1-pre2

--- NuGet/push.bat
+++ NuGet/push.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d %~dp0
-set PACKAGEVERSION=3.2.1-pre
+set PACKAGEVERSION=3.2.1-pre2
 dotnet build ..\Livet.Code.sln -c=Release
-rem API Key ‚Í nuget.org ‚©‚çŽæ“¾‚µ‚Ä nuget setApiKey xxxx ‚ÅƒZƒbƒg‚µ‚Ä‚­‚¾‚³‚¢
+rem Please get an API Key from nuget.org, and set the key using nuget setApiKey xxxx command
 dotnet nuget push ..\LivetCask.Extensions\bin\Release\LivetExtensions.%PACKAGEVERSION%.nupkg -s https://www.nuget.org/api/v2/package
 dotnet nuget push ..\LivetCask.Core\bin\Release\LivetCask.Core.%PACKAGEVERSION%.nupkg -s https://www.nuget.org/api/v2/package
 dotnet nuget push ..\LivetCask.Messaging\bin\Release\LivetCask.Messaging.%PACKAGEVERSION%.nupkg -s https://www.nuget.org/api/v2/package
@@ -14,13 +14,14 @@ dotnet nuget push ..\LivetCask.StatefulModel\bin\Release\LivetCask.StatefulModel
 dotnet nuget push ..\LivetCask\bin\Release\LivetCask.%PACKAGEVERSION%.nupkg -s https://www.nuget.org/api/v2/package
 
 mkdir dist
-copy ..\LivetCask.Extensions\bin\Release\LivetExtensions.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.Core\bin\Release\LivetCask.Core.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.Messaging\bin\Release\LivetCask.Messaging.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.EventListeners\bin\Release\LivetCask.EventListeners.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.Converters\bin\Release\LivetCask.Converters.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.Behaviors\bin\Release\LivetCask.Behaviors.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.Mvvm\bin\Release\LivetCask.Mvvm.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask.StatefulModel\bin\Release\LivetCask.StatefulModel.%PACKAGEVERSION%.nupkg .\dist
-copy ..\LivetCask\bin\Release\LivetCask.%PACKAGEVERSION%.nupkg .\dist
+mkdir dist\%PACKAGEVERSION%
+copy ..\LivetCask.Extensions\bin\Release\LivetExtensions.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.Core\bin\Release\LivetCask.Core.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.Messaging\bin\Release\LivetCask.Messaging.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.EventListeners\bin\Release\LivetCask.EventListeners.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.Converters\bin\Release\LivetCask.Converters.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.Behaviors\bin\Release\LivetCask.Behaviors.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.Mvvm\bin\Release\LivetCask.Mvvm.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask.StatefulModel\bin\Release\LivetCask.StatefulModel.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
+copy ..\LivetCask\bin\Release\LivetCask.%PACKAGEVERSION%.nupkg .\dist\%PACKAGEVERSION%
  
