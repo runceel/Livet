89d45541f00789ca59e12a526cb660d0a1d43b62
soi013 <soi2flatline@gmail.com>
2020-05-12 17:58:46 +0900

    add CursorType to Boolean demo

---
+++ Samples/ViewLayerSupport/Views/CursorTypeToBooleanConverterWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ViewLayerSupport.Views
+{
+    /// <summary>
+    /// CursorTypeToBooleanConverterWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CursorTypeToBooleanConverterWindow : Window
+    {
+        public CursorTypeToBooleanConverterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeWait_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+        }
+        private void ChangeCross_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Cross;
+        }
+    }
+}
