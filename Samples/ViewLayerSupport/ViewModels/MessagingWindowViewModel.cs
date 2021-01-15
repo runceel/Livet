7327bbc4d0e840c66a80e118b4f8fba28ef0521f
soi013 <soi2flatline@gmail.com>
2021-01-15 23:00:59 +0900

    Add File and Folder Dialog demo

--- Samples/ViewLayerSupport/ViewModels/MessagingWindowViewModel.cs
+++ Samples/ViewLayerSupport/ViewModels/MessagingWindowViewModel.cs
@@ -76,11 +76,25 @@ namespace ViewLayerSupport.ViewModels
             OutputMessage = $"{DateTime.Now}: ConfirmFromView: {message.Response ?? false}";
         }
 
+        public void FileSelected([NotNull] OpeningFileSelectionMessage message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+
+
+            string selectedPaths = message.Response == null
+                ? "未選択"
+                : String.Join(";", message.Response);
+            OutputMessage = $"{DateTime.Now}: FileSelected: {selectedPaths}";
+        }
         public void FolderSelected([NotNull] FolderSelectionMessage message)
         {
             if (message == null) throw new ArgumentNullException(nameof(message));
 
-            OutputMessage = $"{DateTime.Now}: FolderSelected: {message.Response ?? "未選択"}";
+            string selectedPaths = message.Response == null
+                ? "未選択"
+                : String.Join(";", message.SelectedPaths);
+
+            OutputMessage = $"{DateTime.Now}: FolderSelected: {selectedPaths}";
         }
 
         public void Initialize() { }
